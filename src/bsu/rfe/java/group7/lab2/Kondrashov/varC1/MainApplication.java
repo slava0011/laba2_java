a9a9d1ee42c7a2d9e2e7f132219ae95da22c2114
slava0011 <mr.slawa2003@mail.ru>
2021-12-12 19:50:25 +0300

    added main class

---
+++ src/bsu/rfe/java/group7/lab2/Kondrashov/varC1/MainApplication.java
@@ -0,0 +1,12 @@
+package bsu.rfe.java.group7.lab2.Kondrashov.varC1;
+
+import javax.swing.*;
+
+public class MainApplication {
+    public static void main(String[]args)
+    {
+        Main_Frame frame = new Main_Frame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
