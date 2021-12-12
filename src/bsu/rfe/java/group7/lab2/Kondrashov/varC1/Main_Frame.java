0c0fa49a0906817e46fda663506bac3767ff4219
slava0011 <mr.slawa2003@mail.ru>
2021-12-12 19:46:04 +0300

    added default variables

--- src/bsu/rfe/java/group7/lab2/Kondrashov/varC1/Main_Frame.java
+++ src/bsu/rfe/java/group7/lab2/Kondrashov/varC1/Main_Frame.java
@@ -10,5 +10,23 @@ import javax.swing.*;
 import static java.lang.Math.*;
 
 public class Main_Frame extends JFrame {
-    
+    private static final int width = 1000;
+    private static final int height = 800;
+    private JTextField result_field;
+    private JTextField x_field;
+    private JTextField y_field;
+    private JTextField z_field;
+    private JLabel image;
+    private ButtonGroup radioButtons = new ButtonGroup();
+    private Box formula_type = Box.createHorizontalBox();
+    private ButtonGroup radioMemoryButtons = new ButtonGroup();
+    private Box hBoxMemoryType = Box.createHorizontalBox();
+    private JTextField memoryTextField;
+    private int formula_number = 1;
+    private int memoryId= 1;
+
+    private Double mem1 = new Double(0);
+    private Double mem2 = new Double(0);
+    private Double mem3 = new Double(0);
+
 }
