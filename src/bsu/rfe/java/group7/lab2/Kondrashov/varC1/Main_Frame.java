61ddf73528c7f62a5fe54d2c7bb0e34bb099f527
slava0011 <mr.slawa2003@mail.ru>
2021-12-12 19:44:17 +0300

    added Main_Frame class and 2 formul

---
+++ src/bsu/rfe/java/group7/lab2/Kondrashov/varC1/Main_Frame.java
@@ -0,0 +1,14 @@
+package bsu.rfe.java.group7.lab2.Kondrashov.varC1;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+import static java.lang.Math.*;
+
+public class Main_Frame extends JFrame {
+    
+}
